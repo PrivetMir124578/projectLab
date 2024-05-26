b928f7f76ae8f185616eab7208255fff0df3a9a1
yanis <yanis.litovnik@yandex.ru>
2024-05-26 13:00:59 +0300

    начало работы на сервисов тестирования бетона. Набросал структуру.

--- ConcreteTesting/src/main/java/com/example/concreteTesting/ConcreteTestingApplication.java
+++ ConcreteTesting/src/main/java/com/example/concreteTesting/ConcreteTestingApplication.java
@@ -1,4 +1,4 @@
-package com.example.concretetesting;
+package com.example.concreteTesting;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
